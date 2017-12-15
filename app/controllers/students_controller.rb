8c8ba92b393aec1d979cd3f2e5ff08a08cf82bd6
Justin Tooley <jstooley823@gmail.com>
2017-12-15 20:01:42 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,6 +16,7 @@ class StudentsController < ApplicationController
   end
 
   def create
+    binding.pry
     @student = Student.new(student_params(:first_name,:last_name))
   	@student.save
   	redirect_to student_path(@student)
