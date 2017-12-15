7f51a6ae5d4ca0922d5b002388c1dfd95612f9c1
Justin Tooley <jstooley823@gmail.com>
2017-12-15 19:01:39 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   private
 
-	def params(*args)
+	def check_params(*args)
   	params.require(:student || :school_class).permit(*args)
 	end
 end
