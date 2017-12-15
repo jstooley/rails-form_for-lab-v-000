0e485d121e7ed2e835a52f5755bd1289f9dbdaf9
Justin Tooley <jstooley823@gmail.com>
2017-12-15 19:52:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  private
-
-	def check_params(*args)
-  	 if params.require(:student)
-       params.require(:student).permit(*args)
-     else
-       params.require(:school_class).permit(*args)
-    end
-	end
-  
 end
