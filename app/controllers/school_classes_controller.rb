6bb38a5876d8745c0212dc1960870133d823103f
Justin Tooley <jstooley823@gmail.com>
2017-12-15 19:38:41 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,6 +12,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    require.pry
     @school_class = SchoolClass.new(school_class_params(:first_name,:last_name))
     @school_class.save
     redirect_to school_class_path(@school_class)
