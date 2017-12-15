5a3b8edf4a2066efde84f64f7cc70f32246f1d37
Justin Tooley <jstooley823@gmail.com>
2017-12-15 19:42:39 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -8,7 +8,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def show
-    @school_class = SchoolClass.find(id: params[:id])
+    @school_class = SchoolClass.find(id: params[:id][:id])
   end
 
   def create
