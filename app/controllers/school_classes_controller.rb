be88ab88f1cb9894aed5d7d3c16bdf0c49b899dc
Justin Tooley <jstooley823@gmail.com>
2017-12-15 19:23:38 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,14 +12,19 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(check_params(:first_name,:last_name))
+    @school_class = SchoolClass.new(school_class_params(:first_name,:last_name))
     @school_class.save
     redirect_to post_path(@school_class)
   end
 
   def update
     @school_class = SchoolClass.find(params[:id])
-    @school_class.update(check_params(:first_name,:last_name))
+    @school_class.update(school_class_params(:first_name,:last_name))
     redirect_to post_path(@school_class)
   end
+
+  private
+  def school_class_params(*args)
+  	params.require(:student || :school_class).permit(*args)
+	end
 end
